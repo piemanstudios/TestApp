61c4160afb96ade5f7d767445a3dff550586fbb0
Matthew Stephens <Matt@Matthew-Stephenss-iMac.local>
2012-01-20 08:47:46 +0000

    Initial Commit

---
+++ TestApp/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  TestApp
+//
+//  Created by Matthew Stephens on 20/01/2012.
+//  Copyright (c) 2012 Pieman Studios Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Matthew Stephens on 20/01/2012.
+//  Copyright (c) 2012 Pieman Studios Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
